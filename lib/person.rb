17d7b0cf62105961a020bcd3bc62795336e09880
Charlie Knight <williamchknight@gmail.com>
2020-11-18 10:27:53 -0800

    halfway

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,44 @@
 # your code goes here
+class Person
+    
+    attr_reader :name 
+    attr_accessor :bank_account, :happiness, :hygiene
+   
+    
+    def initialize(name)
+        @name = name
+        @bank_account = 25
+        @happiness = 8
+        @hygiene = 8
+    end     
+    
+    def clean?
+        if @hygiene > 7 
+            true
+        else
+            false
+        end
+    end
+
+    def happy?
+        if @happiness > 7 
+            true
+        else
+            false
+        end
+    end
+
+    def get_paid(salary)
+        @bank_account += salary
+        p "all about the benjamins" 
+    end    
+
+    def take_bath
+        @hygiene=@hygiene + 4 
+        if @hygiene > 10
+            @hygiene = 10
+        end
+        p "♪ Rub-a-dub just relaxing in the tub ♫"
+    end     
+end 
+
